6314a1217581b9b391e6c2f81aa54504ed75ab48
George Gkonis <georgegkonis@outlook.com>
2023-09-20 18:06:55 +0300

    refactor: use status-code enum

--- backend/src/middleware/require-user.ts
+++ backend/src/middleware/require-user.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import AppError from '../errors/app-error';
+import { StatusCode } from '../enums/status-code.enum';
 
 export const requireUser = (
     req: Request,
@@ -9,9 +10,8 @@ export const requireUser = (
     try {
         const user = res.locals.user;
         if (!user) {
-            return next(new AppError(`Invalid token or session has expired`, 401));
+            return next(new AppError(`Invalid token or session has expired`, StatusCode.UNAUTHORIZED));
         }
-
         next();
     } catch (err: any) {
         next(err);
