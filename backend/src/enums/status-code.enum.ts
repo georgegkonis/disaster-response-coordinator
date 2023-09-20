44b0637732f0de61a70114efa1798993772cb544
George Gkonis <georgegkonis@outlook.com>
2023-09-20 16:25:51 +0300

    feature: add http status codes

---
+++ backend/src/enums/status-code.enum.ts
@@ -0,0 +1,10 @@
+export enum StatusCode {
+    OK = 200,
+    CREATED = 201,
+    NO_CONTENT = 204,
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    NOT_FOUND = 404,
+    CONFLICT = 409,
+    SERVER_ERROR = 500
+}
