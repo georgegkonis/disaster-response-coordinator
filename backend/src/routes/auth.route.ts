1ae4808c31198a49bc8225a882439b2decbb5b49
George Gkonis <georgegkonis@outlook.com>
2023-09-20 18:10:03 +0300

    refactor: rename to 'register'

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import { loginHandler, registerHandler } from '../controllers/auth.controller';
 import { validate } from '../middleware/validate';
-import { createUserSchema, loginUserSchema } from '../schemas/user.schema';
+import { registerUserSchema, loginUserSchema } from '../schemas/user.schema';
 
 const router = express.Router();
 
 // Register user route
-router.post('/register', validate(createUserSchema), registerHandler);
+router.post('/register', validate(registerUserSchema), registerHandler);
 
 // Login user route
 router.post('/login', validate(loginUserSchema), loginHandler);
