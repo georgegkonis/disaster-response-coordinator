7789530f959ba89300b310a185522655328aec79
George Gkonis <georgegkonis@outlook.com>
2023-10-08 22:42:18 +0300

    feat: authenticate user before logout

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { loginHandler, logoutHandler, registerHandler } from '../controllers/auth.controller';
 import { validate } from '../middleware/validate';
 import { registerUserSchema, loginUserSchema } from '../schemas/user.schema';
+import { deserializeUser } from '../middleware/deserialize-user';
+import { requireUser } from '../middleware/require-user';
 
 const router = express.Router();
 
@@ -12,6 +14,6 @@ router.post('/register', validate(registerUserSchema), registerHandler);
 router.post('/login', validate(loginUserSchema), loginHandler);
 
 // Logout user route
-router.post('/logout', logoutHandler);
+router.post('/logout', deserializeUser, requireUser, logoutHandler);
 
 export default router;
