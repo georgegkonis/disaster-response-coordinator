e1a8b7ef45461352999a7f2601780b01ffc63564
George Gkonis <georgegkonis@outlook.com>
2023-10-08 15:39:46 +0300

    feat: don't validate password length on login

--- backend/src/schemas/user.schema.ts
+++ backend/src/schemas/user.schema.ts
@@ -22,7 +22,6 @@ export const loginUserSchema = object({
     body: object({
         username: string({ required_error: 'Username is required' }),
         password: string({ required_error: 'Password is required' })
-            .min(8, 'Invalid email or password')
     })
 });
 
