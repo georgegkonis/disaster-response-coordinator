de05e34603e7bb44fa0c4078bf77afe1949e441e
George Gkonis <georgegkonis@outlook.com>
2023-10-09 12:13:31 +0300

    feat: display and edit the profile

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,37 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UpdateUserRequest } from '../../models/requests.model';
 import { AppState } from '../../store/app.reducer';
 import { Store } from '@ngrx/store';
-import { AuthActions, UserActions } from '../../store/app.actions';
+import { UserActions } from '../../store/app.actions';
+import { Router } from '@angular/router';
 import { selectCurrentUser } from '../../store/app.selector';
 import { Subscription } from 'rxjs';
-import { User } from '../../models/user.model';
 
 @Component({
     selector: 'app-profile',
     templateUrl: './profile.component.html',
     styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
+    profileForm!: FormGroup;
 
     private subscription: Subscription = new Subscription();
 
+    private currentUserSubscription = () => this.store.select(selectCurrentUser)
+        .subscribe(user => {
+            if (user) {
+                this.profileForm.patchValue(user);
+            }
+        });
+
     constructor(
-        private store: Store<AppState>
-    ) {}
+        private formBuilder: FormBuilder,
+        private store: Store<AppState>,
+        private router: Router
+    ) { }
 
     ngOnInit(): void {
         this.store.dispatch(UserActions.getCurrent());
-        this.store.dispatch(UserActions.getAll());
+
+        this.profileForm = this.formBuilder.group({
+            username: [null],
+            email: [{ value: '', disabled: true }],
+            password: [null]
+        });
 
         this.subscription.add(this.currentUserSubscription());
     }
 
-    private currentUserSubscription = (): Subscription => this.store.select(selectCurrentUser)
-        .subscribe((user: User | null) => {
-            if (user) console.log(user);
-        });
-
-    onLogoutClick(): void {
-        this.store.dispatch(AuthActions.logout());
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
     }
+
+    onSubmit(): void {
+        if (this.profileForm.dirty) {
+            const request: UpdateUserRequest = {
+                username: this.profileForm.value.username ?? undefined,
+                password: this.profileForm.value.password ?? undefined
+            };
+            this.store.dispatch(UserActions.updateCurrent(request));
+        }
+        this.router.navigate(['/dashboard']).then();
+    }
+
+    onCancelClick(): void {
+        this.router.navigate(['/dashboard']).then();
+    }
+
+
 }
