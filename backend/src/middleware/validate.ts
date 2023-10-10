f7274c1c876c35e8f81a76758db3a2c40ae32334
George Gkonis <georgegkonis@outlook.com>
2023-10-10 04:13:07 +0300

    style: reformat

--- backend/src/middleware/validate.ts
+++ backend/src/middleware/validate.ts
@@ -3,29 +3,31 @@ import { AnyZodObject, ZodError } from 'zod';
 import AppError from '../errors/app-error';
 import { StatusCode } from '../enums/status-code.enum';
 
-export const validate =
-    (schema: AnyZodObject) =>
-        (req: Request, res: Response, next: NextFunction) => {
-            try {
-                schema.parse({
-                    params: req.params,
-                    query: req.query,
-                    body: req.body
-                });
+export const validate = (schema: AnyZodObject) => (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        schema.parse({
+            params: req.params,
+            query: req.query,
+            body: req.body
+        });
 
-                next();
-            } catch (err: any) {
-                if (err instanceof ZodError) {
-                    let messages = err.errors.map(error => error.message);
+        next();
+    } catch (err: any) {
+        if (err instanceof ZodError) {
+            let messages = err.errors.map(error => error.message);
 
-                    const appError = new AppError(
-                        'Validation failed',
-                        StatusCode.BAD_REQUEST,
-                        messages
-                    );
-                    next(appError);
-                }
-                next(err);
-            }
-        };
+            const appError = new AppError(
+                'Validation failed',
+                StatusCode.BAD_REQUEST,
+                messages
+            );
+            next(appError);
+        }
+        next(err);
+    }
+};
 
