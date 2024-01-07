df2eb7a4faf57cff45723be62c33d7069478441f
George Gkonis <georgegkonis+1password@outlook.com>
2024-01-07 15:01:59 +0200

    feat: update the error handler to include the error details

--- backend/src/middleware/handle-errors.ts
+++ backend/src/middleware/handle-errors.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { StatusCode } from '../enums/status-code.enum';
 import AppError from '../errors/app-error';
+ import { Status } from '../enums/status.enum';
 
 export function handleErrors(
     err: AppError | any,
@@ -8,8 +9,16 @@ export function handleErrors(
     res: Response,
     _next: NextFunction
 ) {
-    err.status = err.status || 'error';
+    err.status = err.status || Status.ERROR;
     err.statusCode = err.statusCode || StatusCode.SERVER_ERROR;
+
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+            additionalInfo: err.additionalInfo
+        });
+    }
 
     res.status(err.statusCode).json({
         status: err.status,
