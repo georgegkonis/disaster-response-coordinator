710828bc0f02acb99930ee78c9309d64064e9b95
George Gkonis <georgegkonis@outlook.com>
2024-02-11 19:52:14 +0200

    refactor: remove dual param

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -7,7 +7,7 @@ export const signJwt = (payload: Object, options: SignOptions = {}) => {
         'base64'
     ).toString('ascii');
     return jwt.sign(payload, privateKey, {
-        ...(options && options),
+        ...options,
         algorithm: 'RS256'
     });
 };
