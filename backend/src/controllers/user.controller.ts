464962d1385b4444097ceafecab7803eef4a9c4e
George Gkonis <georgegkonis@outlook.com>
2024-02-11 19:25:52 +0200

    refactor: reorganize code

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -7,6 +7,51 @@ import { CreateUserInput, UpdateUserInput, UpdateUserLocationInput } from '../sc
 import { Role } from '../enums/role.enum';
 import ConflictError from '../errors/conflict.error';
 
+export const createUserHandler = async (
+    req: Request<{}, {}, CreateUserInput>,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const user = await createUser(req.body);
+
+        res.status(StatusCode.CREATED).json(user);
+    } catch (err: any) {
+        if (err.code === MongoErrorCodes.DUPLICATE_KEY) {
+            err = new ConflictError('Username or email already exist');
+        }
+        next(err);
+    }
+};
+
+export const getUsersHandler = async (
+    req: Request<{ role?: Role }>,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const users = await findUsers(req.query);
+
+        res.status(StatusCode.OK).json(users);
+    } catch (err: any) {
+        next(err);
+    }
+};
+
+export const getUserHandler = async (
+    req: Request<{ id: string }>,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const user = await getUser(req.params.id);
+
+        res.status(StatusCode.OK).json(user);
+    } catch (err: any) {
+        next(err);
+    }
+};
+
 export const getMeHandler = (
     _req: Request,
     res: Response,
@@ -27,10 +72,10 @@ export const updateMeHandler = async (
     next: NextFunction
 ) => {
     try {
-        const id: string = res.locals.user._id;
-        const user = await updateUser(id, req.body);
+        const userId: string = res.locals.user._id;
 
-        updateUserCache(id, user);
+        const user = await updateUser(userId, req.body);
+        updateUserCache(userId, user);
 
         res.status(StatusCode.OK).json(user);
     } catch (err: any) {
@@ -47,10 +92,25 @@ export const updateMyLocationHandler = async (
     next: NextFunction
 ) => {
     try {
-        const id: string = res.locals.user._id;
+        const userId: string = res.locals.user._id;
 
-        await updateUser(id, { location: req.body });
-        updateUserCache(id, { location: req.body });
+        const user = await updateUser(userId, { location: req.body });
+        updateUserCache(userId, user);
+
+        res.status(StatusCode.NO_CONTENT).json();
+    } catch (err: any) {
+        next(err);
+    }
+};
+
+export const deleteUserHandler = async (
+    req: Request<{ id: string }>,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        await deleteUser(req.params.id);
+        deleteUserCache(req.params.id);
 
         res.status(StatusCode.NO_CONTENT).json();
     } catch (err: any) {
@@ -64,75 +124,13 @@ export const deleteMeHandler = async (
     next: NextFunction
 ) => {
     try {
-        const userId = res.locals.user._id.toString();
+        const userId = res.locals.user._id;
 
         res.clearCookie('accessToken');
         res.clearCookie('loggedIn');
 
-        deleteUserCache(userId);
-
         await deleteUser(userId);
-
-        res.status(StatusCode.NO_CONTENT).json();
-    } catch (err: any) {
-        next(err);
-    }
-};
-
-export const createUserHandler = async (
-    req: Request<{}, {}, CreateUserInput>,
-    res: Response,
-    next: NextFunction
-) => {
-    try {
-        await createUser(req.body);
-
-        res.status(StatusCode.NO_CONTENT).json();
-    } catch (err: any) {
-        if (err.code === MongoErrorCodes.DUPLICATE_KEY) {
-            err = new ConflictError('Username or email already exist');
-        }
-        next(err);
-    }
-};
-
-export const getUserHandler = async (
-    req: Request<{ id: string }>,
-    res: Response,
-    next: NextFunction
-) => {
-    try {
-        const user = await getUser(req.params.id);
-
-        res.status(StatusCode.OK).json(user);
-    } catch (err: any) {
-        next(err);
-    }
-};
-
-export const getUsersHandler = async (
-    req: Request<{ role?: Role }>,
-    res: Response,
-    next: NextFunction
-) => {
-    try {
-        const users = await findUsers(req.query);
-
-        res.status(StatusCode.OK).json(users);
-    } catch (err: any) {
-        next(err);
-    }
-};
-
-export const deleteUserHandler = async (
-    req: Request<{ id: string }>,
-    res: Response,
-    next: NextFunction
-) => {
-    try {
-        await deleteUser(req.params.id);
-
-        deleteUserCache(req.params.id);
+        deleteUserCache(userId);
 
         res.status(StatusCode.NO_CONTENT).json();
     } catch (err: any) {
