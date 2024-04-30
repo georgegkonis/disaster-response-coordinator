7f8ca836e96ca3a4d2f4aa78a10bec038e3879c9
George Gkonis <georgegkonis@outlook.com>
2024-04-30 23:04:13 +0300

    feat: improve the `NotFoundError`

--- backend/src/errors/not-found-error.ts
+++ backend/src/errors/not-found-error.ts
@@ -3,8 +3,10 @@ import AppError from './app-error';
 
 export default class NotFoundError extends AppError {
 
-    constructor(name: string, additionalInfo: string | string[]) {
-        if (Array.isArray(additionalInfo)) {
+    constructor(name: string, additionalInfo: string | string[] = '') {
+        if (additionalInfo === '') {
+            super(`The ${name} could not be found`, StatusCode.NOT_FOUND);
+        } else if (Array.isArray(additionalInfo)) {
             super(`Some ${name} could not be found`, StatusCode.NOT_FOUND, additionalInfo);
         } else {
             super(`The ${name} with ID '${additionalInfo}' could not be found`, StatusCode.NOT_FOUND);
