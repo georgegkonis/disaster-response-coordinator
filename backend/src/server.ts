c68a02ce06476208a00e11a0403c60c3b1620ba6
George Gkonis <georgegkonis@outlook.com>
2024-04-30 22:57:09 +0300

    docs: delete comment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -69,6 +69,5 @@ app.use(handleErrors);
 const port = config.get<number>('port');
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
-    // ? call the connectDB function here
     connectDB();
 });
