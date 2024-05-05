6be5e29bf452350d45e2fa4261697208abbebfb3
George Gkonis <georgegkonis@outlook.com>
2024-05-05 12:18:56 +0300

    feat(frontend): implement `AnnouncementService` and add needed request DTOs

---
+++ frontend/src/app/services/announcement.service.ts
@@ -0,0 +1,39 @@
+import { Inject, Injectable } from '@angular/core';
+import { APP_SETTINGS, AppSettings } from '../settings/settings';
+import { HttpClient } from '@angular/common/http';
+import { Announcement } from '../models/announcement.model';
+import { Observable } from 'rxjs';
+import { CreateAnnouncementRequest } from '../dto/requests/create-announcement-request.dto';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AnnouncementService {
+
+    private readonly baseUrl: string = '';
+
+    constructor(
+        @Inject(APP_SETTINGS) settings: AppSettings,
+        private httpClient: HttpClient
+    ) {
+        this.baseUrl = `${settings.apiUrl}/announcements`;
+    }
+
+    create(request: CreateAnnouncementRequest): Observable<Announcement> {
+        const url: string = `${this.baseUrl}`;
+
+        return this.httpClient.post<Announcement>(url, request);
+    }
+
+    getAll(): Observable<Announcement[]> {
+        const url: string = `${this.baseUrl}`;
+
+        return this.httpClient.get<Announcement[]>(url);
+    }
+
+    delete(id: string): Observable<void> {
+        const url: string = `${this.baseUrl}/${id}`;
+
+        return this.httpClient.delete<void>(url);
+    }
+}
