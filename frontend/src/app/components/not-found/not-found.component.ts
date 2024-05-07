c8703f906a9b48a0f2024131b0bed53ceb8ca7d9
George Gkonis <georgegkonis@outlook.com>
2024-05-07 20:03:57 +0300

    feat(frontend): improve look of not found page

--- frontend/src/app/components/not-found/not-found.component.ts
+++ frontend/src/app/components/not-found/not-found.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { routesPaths } from '../../constants/routes-paths';
 
 @Component({
-  selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+    selector: 'app-not-found',
+    templateUrl: './not-found.component.html',
+    styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+
+    protected readonly routesPaths = routesPaths;
+}
