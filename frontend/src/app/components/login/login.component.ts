56afd6c09e41af6cc4eee00b48e95b5aad228314
George Gkonis <georgegkonis@outlook.com>
2024-05-07 19:58:16 +0300

    feat(frontend): redirect directly to dashboard if the user is already logged in

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store/app.reducer';
 import { AuthActions } from '../../store/app.actions';
 import { LoginRequest } from '../../dto/requests/login-request.dto';
 import { NavigationService } from '../../services/navigation.service';
+import { CookieService } from 'ngx-cookie-service';
 
 interface LoginForm {
     username: FormControl<string>;
@@ -16,7 +17,7 @@ interface LoginForm {
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
     protected readonly loginForm: FormGroup<LoginForm> = new FormGroup<LoginForm>({
         username: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
@@ -25,8 +26,15 @@ export class LoginComponent {
 
     constructor(
         private store: Store<AppState>,
-        private navigationService: NavigationService
+        private navigationService: NavigationService,
+        private cookieService: CookieService
     ) {}
+
+    ngOnInit(): void {
+        if (this.cookieService.check('loggedIn')) {
+            this.navigationService.navigateToDashboard();
+        }
+    }
 
     onSubmit(): void {
         if (this.loginForm.valid) {
