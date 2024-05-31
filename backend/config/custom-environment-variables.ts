2d33f424e028b7ed3d83baa3ada2c6cc05cb03ac
George Gkonis <georgegkonis@outlook.com>
2024-05-31 15:28:18 +0300

    fix: use correct env variable name

--- backend/config/custom-environment-variables.ts
+++ backend/config/custom-environment-variables.ts
@@ -2,7 +2,7 @@ export default {
     db: {
         username: 'MONGODB_USERNAME',
         password: 'MONGODB_PASSWORD',
-        database: 'MONGODB_DATABASE_NAME',
+        database: 'MONGODB_DATABASE',
     },
     accessTokenPrivateKey: 'ACCESS_TOKEN_PRIVATE_KEY',
     accessTokenPublicKey: 'ACCESS_TOKEN_PUBLIC_KEY'
