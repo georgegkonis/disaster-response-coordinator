2e88fd146837b88323fd3dd5d96d3cc06ba0042c
George Gkonis <georgegkonis+github@outlook.com>
2024-06-26 15:00:12 +0300

    feat(frontend): add dashboard profile icon

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -6,6 +6,10 @@ import { CookieService } from 'ngx-cookie-service';
 import { routesPaths } from '../../constants/routes-paths';
 import { AppState } from '../../store/reducers/app.reducer';
 import { AuthActions } from '../../store/actions/auth.actions';
+import { User } from '../../models/user.model';
+import { Observable } from 'rxjs';
+import { userSelector } from '../../store/selectors/app.selector';
+import { UserActions } from '../../store/actions/user.actions';
 
 @Component({
     selector: 'app-dashboard',
@@ -14,18 +18,19 @@ import { AuthActions } from '../../store/actions/auth.actions';
 })
 export class DashboardComponent implements OnInit {
 
-    private readonly isAdmin: boolean;
+    protected readonly routesPaths = routesPaths;
+    protected readonly user$: Observable<User | null>;
 
-    protected menuItems!: MenuItem[];
+    protected menuItems: MenuItem[];
 
     constructor(
         private store: Store<AppState>,
         cookieService: CookieService
     ) {
-        this.isAdmin = cookieService.get('userRole') === UserRole.ADMIN;
-    }
+        this.user$ = this.store.select(userSelector);
 
-    ngOnInit(): void {
+        const isAdmin: boolean = cookieService.get('userRole') === UserRole.ADMIN;
+
         this.menuItems = [
             {
                 label: 'Map',
@@ -36,7 +41,13 @@ export class DashboardComponent implements OnInit {
                 label: 'Warehouse',
                 icon: 'pi pi-fw pi-home',
                 routerLink: routesPaths.WAREHOUSE,
-                visible: this.isAdmin,
+                visible: isAdmin,
+                items: [
+                    {
+                        label: 'Categories',
+                        icon: 'pi pi-fw pi-tags'
+                    }
+                ]
             },
             {
                 label: 'Announcements',
@@ -47,31 +58,23 @@ export class DashboardComponent implements OnInit {
                 label: 'Analytics',
                 icon: 'pi pi-fw pi-chart-bar',
                 routerLink: routesPaths.ANALYTICS,
-                visible: this.isAdmin,
+                visible: isAdmin,
                 disabled: true
             },
             {
                 label: 'Users',
                 icon: 'pi pi-fw pi-users',
                 routerLink: routesPaths.USERS,
-                visible: this.isAdmin
-            },
-            {
-                label: 'Account',
-                icon: 'pi pi-fw pi-user',
-                items: [
-                    {
-                        label: 'Details',
-                        icon: 'pi pi-fw pi-user-edit',
-                        routerLink: routesPaths.PROFILE
-                    },
-                    {
-                        label: 'Logout',
-                        icon: 'pi pi-fw pi-sign-out',
-                        command: () => this.store.dispatch(AuthActions.logout())
-                    }
-                ]
+                visible: isAdmin
             }
         ];
+    }
+
+    ngOnInit(): void {
+        this.store.dispatch(UserActions.loadMe());
+    }
+
+    onLogoutClick(): void {
+        this.store.dispatch(AuthActions.logout());
     }
 }
