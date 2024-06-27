623b427f2edc759b4bada8e18e4fb0968664f1bb
George Gkonis <georgegkonis@outlook.com>
2024-06-27 08:05:19 +0300

    feat(frontend): add loader for warehouse effects

--- frontend/src/app/store/effects/warehouse.effects.ts
+++ frontend/src/app/store/effects/warehouse.effects.ts
@@ -5,30 +5,37 @@ import { WarehouseActions } from '../actions/warehouse.actions';
 import { WarehouseService } from '../../services/warehouse.service';
 import { Injectable } from '@angular/core';
 import { saveAs } from 'file-saver';
+import { withMinDelay } from '../../utilities/with-min-delay';
+import { AppLoaderService } from '../../services/app-loader.service';
 
 @Injectable()
 export class WarehouseEffects {
 
     constructor(
         private actions$: Actions,
-        private warehouseService: WarehouseService
+        private warehouseService: WarehouseService,
+        private loaderService: AppLoaderService
     ) {}
 
     importDataEffect$ = createEffect(() => this.actions$.pipe(
         ofType(WarehouseActions.importData),
-        mergeMap(({ file }) => this.warehouseService.importData(file).pipe(
+        tap(() => this.loaderService.show()),
+        mergeMap(({ file }) => withMinDelay(this.warehouseService.importData(file)).pipe(
             map(() => WarehouseActions.importDataSuccess()),
             catchError(() => of(WarehouseActions.importDataFailure()))
-        ))
+        )),
+        tap(() => this.loaderService.hide())
     ));
 
     exportDataEffect$ = createEffect(() => this.actions$.pipe(
         ofType(WarehouseActions.exportData),
-        mergeMap(() => this.warehouseService.exportData().pipe(
-            tap((data) => saveAs(data, 'warehouse.json')),
+        tap(() => this.loaderService.show()),
+        mergeMap(() => withMinDelay(this.warehouseService.exportData()).pipe(
+            tap((data) => saveAs(data, 'warehouse_data.json')),
             map((data) => WarehouseActions.exportDataSuccess({ data })),
             catchError(() => of(WarehouseActions.exportDataFailure()))
-        ))
+        )),
+        tap(() => this.loaderService.hide())
     ));
 
     clearDataEffect$ = createEffect(() => this.actions$.pipe(
